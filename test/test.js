37b3439262df0110b99567f6f5cb38821518ffde
Dmitriy Golovachev <d.a.golovachev@gmail.com>
2019-10-27 20:47:20 +0600

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,13 @@
+const Chat2DeskClient = require('../lib/chat2deskClient');
+
+const TOKEN = process.env.TOKEN;
+
+const chat2DeskClient = new Chat2DeskClient(TOKEN);
+
+chat2DeskClient.GetMessages(0, 50)
+    .then(response => {
+        let messages = response.data;
+        for (let message of messages) {
+            console.log(`text: ${message.text}, date: ${message.created}, transport: ${message.transport}`)
+        }
+    }).catch(error => console.log(error));
